8c78a157c48087e49bcd972db8cad8871ea75931
ahmedmaged0a <ahmedmaged0a@gmail.com>
2025-05-16 14:44:49 +0300

    Creating Factorial Function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-
+long getfactorial(long value);
 void main(void)
 {
 	printf("This is first ITI Repo");
+	long var;
+	printf("Enter a number");
+	scanf("%d",&var);
+	printf("factorial is %d",getfactorial(var));
+}
+long getfactorial(long value)
+{
+	if(value ==0 || value ==1) return 1;
+	else{
+		long res = 1;
+		for(long i = value ;i>=2;--i)
+		{
+			res *=i;
+		}
+		return res;
+	}
+	
 }
